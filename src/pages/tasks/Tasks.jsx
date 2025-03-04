1e123993cc4731910da9d40d746b5da337bf3f44
Kamoliddin Mirzaboyev <Kamoliddinmirzaboyev05@gmail.com>
2025-03-04 15:49:59 +0500

    Update

--- src/pages/tasks/Tasks.jsx
+++ src/pages/tasks/Tasks.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 function Tasks() {
   return (
@@ -6,70 +6,55 @@ function Tasks() {
       <div className="page">
         <div className="homeHero">
           <div className="container">
-            <div className="aboutPlatform">
-              <h2>
-                {" "}
-                Eduway – Kelajak IT Mutaxassislari Uchun Ta`lim Platformasi
-              </h2>
-              <p>
-                Eduway – bu IT kasblarni o‘rganmoqchi bo‘lganlar uchun
-                yaratilgan innovatsion ta’lim platformasi. Platforma orqali
-                yangi boshlovchilardan tortib, tajribali mutaxassislargacha o‘z
-                bilimlarini mustahkamlashlari, yangi texnologiyalarni
-                o‘rganishlari va amaliy tajriba orttirishlari mumkin.
-              </p>
-              <div className="forWhom">
-                <h2>🎯 Kimlar uchun?</h2>{" "}
-                <p>
-                  Dasturlashni o‘rganmoqchi bo‘lganlar – Noldan boshlovchilar
-                  uchun tushunarli va amaliy kurslar.
-                </p>
-                <p>
-                  IT mutaxassislari – Bilimlarini mustahkamlash va yangi
-                  texnologiyalarni o‘rganish imkoniyati.
-                </p>
-                <p>
-                  {" "}
-                  Talabalar – Amaliy topshiriqlar va real loyihalar bilan
-                  tajriba orttirish.
-                </p>
-                <p>
-                  Frilanserlar – Kasbiy mahoratini oshirib, rezyume yaratish
-                  imkoniyati.
-                </p>
+            <div className="courseBlock">
+              <div className="courseBox">
+                <div className="boxImg">
+                  <img src="/public/html.png" alt="" />
+                </div>
+                <h2>HTML Uchun Topshiriqlar</h2>
+                <p>HTML bosqichidagi foydalanuvchilar uchun topshiriqlar.</p>
               </div>
-              <div className="opportunities">
-                <h2>🚀 Eduway'da Sizni Nima Kutmoqda?</h2>
-                <p>
-                  {" "}
-                  ✅ Interaktiv kurslar – Dasturlash, veb-ishlab chiqish, sun’iy
-                  intellekt va boshqa yo‘nalishlarda sifatli ta’lim.
-                </p>
-                <p>
-                  ✅ Topshiriqlar va amaliy mashg‘ulotlar – O‘z bilimlaringizni
-                  sinab ko‘rib, real muammolarni hal qilish tajribasi.
-                </p>
-                <p>
-                  {" "}
-                  ✅ Resurslar va qo‘llanmalar – Foydali maqolalar, darsliklar
-                  va qo‘llanmalar.
-                </p>
-                <p>
-                  ✅ Eduway – zamonaviy IT kasblarni o‘rganish va professional
-                  rivojlanish uchun eng qulay platforma!
-                </p>
-                <p>
-                  {" "}
-                  ✅ Resume Builder – O‘z rezyumengizni tayyorlab, ish
-                  beruvchilarga taqdim etish imkoniyati.
-                </p>
+              <div className="courseBox">
+                <div className="boxImg">
+                  <img src="/public/htmlcss.jpg" alt="" />
+                </div>
+                <h2>HTML + CSS</h2>
+                <p>HTML bosqichidagi foydalanuvchilar uchun topshiriqlar.</p>
+              </div>
+              <div className="courseBox">
+                <div className="boxImg">
+                  <img src="/public/sass.png" alt="" />
+                </div>
+                <h2>HTML + SASS</h2>
+                <p>HTML bosqichidagi foydalanuvchilar uchun topshiriqlar.</p>
+              </div>
+              <div className="courseBox">
+                <div className="boxImg">
+                  <img src="/public/bootstrap.png" alt="" />
+                </div>
+                <h2>HTML + Bootstrap</h2>
+                <p>HTML bosqichidagi foydalanuvchilar uchun topshiriqlar.</p>
+              </div>
+              <div className="courseBox">
+                <div className="boxImg">
+                  <img src="/public/tailwind.png" alt="" />
+                </div>
+                <h2>HTML + Tailwind CSS</h2>
+                <p>HTML bosqichidagi foydalanuvchilar uchun topshiriqlar.</p>
+              </div>
+              <div className="courseBox">
+                <div className="boxImg">
+                  <img src="/public/html.png" alt="" />
+                </div>
+                <h2>HTML Uchun Topshiriqlar</h2>
+                <p>HTML bosqichidagi foydalanuvchilar uchun topshiriqlar.</p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Tasks
+export default Tasks;
